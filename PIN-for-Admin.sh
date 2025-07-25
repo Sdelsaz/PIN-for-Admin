08d46416706ae288e585866293b6b866d17dfee3
Sdelsaz <77850996+Sdelsaz@users.noreply.github.com>
2025-07-25 16:55:26 +0200

    Update PIN-for-Admin.sh
    Updated to use swiftDialog and allow for custom branding

--- PIN-for-Admin.sh
+++ PIN-for-Admin.sh
@@ -14,53 +14,52 @@
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
-# This script is based on Laurent Pertois' Temp Admin script:
-#
-# https://github.com/laurentpertois/Temp-Admin 
-#
-# The script will generate a random PIN, send it to the Jamf Pro Inventory and prompt the user for the PIN.
-# The following can be customized:
-# - The amount of time the user gets Admin Privileges for
-# - The number of PIN attempts allowed.
-# - The length of the PIN
-# 
+# The script will generate a random PIN, and prompt the user for the PIN. An Extension Attribute is used to populate the PIN in the Jamf Pro Inventory
 #
 # Parameters:
 #
 # $4= Amount of time in minutes
 # $5= Maximum number of attempts
 # $6= Number of characters/PIN length
+# $7= Organisation name
+# $8= Path to a custom icon
 #
-# You can set the amount of time for which they have this privilege in Minutes as Parameter 4
-# If not amount of time is set, the default is 10 minutes.
-# 
-# A window will show them the amount of time left, if they close it the script will still execute
-# 
-# After the time is elapsed, their privileges are removed. Any new account that is admin is also
-# demoted except the accounts that were admin before the execution of the script.
+# This script is based on Laurent Pertois' Temp Admin script:
+# https://github.com/laurentpertois/Temp-Admin 
 #
-# You can set the maximum amount of attempts in Parameter 5
-# If no value is set, the default is 3
-#
-# You can set the Length of the PIN in Parameter 6
-# If no value is set, the default is 5 characters
+# This script uses Bart Reardon's swiftDialog for user dialogs:
+# https://github.com/bartreardon/swiftDialog
 #
 # Created by: Sebastien Del Saz Alvarez
 # Updated On: 2022-11-22 to random PIN creation
 # Updated On: 2023-04-22 to allow to set the maximum number of attempts and the PIN Length
-# Updated On: 2024-06-20 to use a hidden local file to store the PIN. This avoids potential issues with recyclinbg existing attributes.
+# Updated On: 2024-06-20 to use a hidden local file to store the PIN. This avoids potential issues with recycling existing attributes.
+# Updated On: 2025-07-25 to use SwiftDialog for the prompts and add custom branding options
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+# Check if Swift Dialog is installed. if not, Install it
+logger "Checking if SwiftDialog is installed"
+if [[ -e "/usr/local/bin/dialog" ]]
+then
+logger "SwiftDialog is already installed"
+else
+logger "SwiftDialog Not installed, downloading and installing"
+/usr/bin/curl https://github.com/swiftDialog/swiftDialog/releases/download/v2.5.5/dialog-2.5.5-4802.pkg -L -o /tmp/dialog-2.5.5-4802.pkg 
+cd /tmp
+/usr/sbin/installer -pkg dialog-2.5.5-4802.pkg -target /
+fi
+
+# Variables:
 # Check if there is a value passed as $4 for the number of minutes, if not, defaults to 10
 if [ -z "$4" ]; then
-	TEMPMINUTES=10
+	TEMPMINUTES=600
 else
 	
 # Check if the value passed as $4 for the number of minutes is a positive numeric number 
 # without any extra characters (i.e. 10, not +10 or -10), if not, defaults to 10
 if [[ "$4" =~ [^0-9]+ ]] ; then
-	TEMPMINUTES=10
+	TEMPMINUTES=600
 else
 	TEMPMINUTES="$4"
 fi
@@ -80,49 +79,81 @@ else
 	PINLength="$6"	
 fi
 
-# Generate a random PIN and populate the attribute of hidden file with the value
+# Check if there is a value passed as $7 for the Organisation Name, if not, set default title
+if [ -z "$7" ]; then
+	OrgName="PIN for Admin"	
+else
+	OrgName="$7"	
+fi
 
+# Check if there is a value passed as $8 for the icon, if not, set default icon
+if [ -z "$8" ]; then
+	Icon="https://i.imgur.com/vJrHrFJ.png"	
+else
+Icon="$8"	
+fi
+
+# Fonts
+MessageFont="size=20,name=PTSans-Regular"
+TitleFont="weight=bold,size=30,name=PTSans-Regular"
+
+# Prompts
+PINPrompt()
+{
+UserPin=$(dialog --small --title "$OrgName" --titlefont "$TitleFont" --message "Please enter the PIN provided by IT." --messagefont "$MessageFont" --icon "$Icon" --alignment "left" --textfield "PIN","secure" : true --button2 --alignment "left" --height "30%")
+if [ $? == 0 ]
+then
+UserPin=$(echo "$UserPin" | awk -F ': ' '{print $2}')
+echo $UserPin
+else
+echo "User cancelled"
+exit 0
+fi
+}
+
+IncorrectPINPrompt()
+{
+	dialog --small --title "$OrgName" --titlefont "$TitleFont" --message "Incorrect PIN provided too many times.\n\n Please contact IT to obtain a PIN." --icon "$Icon" --messagefont "$MessageFont" --button2 --alignment "left" --height "30%" --witdh "40%"
+}
+
+AlreadyAdminPrompt()
+{
+	dialog -s --title "$OrgName" --titlefont "$TitleFont" --message "You already have elevated privileges." --icon "$Icon" --messagefont "$MessageFont" --button2 --alignment "left" --height "30%" --witdh "40%"
+}
+
+ElevationCompletePrompt()
+{
+	dialog -s --title "$OrgName" --titlefont "$TitleF0nt" --message "You now have temporary Administrator Privileges.  \n  \nYou can close this window without affecting your temporary elevation." --icon "$Icon" --messagefont "$MessageFont" --timer $TEMPMINUTES --button1text "Close" --alignment "left" --height "40%" --witdh "40%" --moveable --position "topright"
+}
+
+# Generate a random PIN and populate the attribute of hidden file with the value
 PIN=$(printf '%0'$PINLength'd\n' $((1 + RANDOM % 1000000)))
 
 # write PIN to hidden file
-
 touch /usr/local/.PIN.txt
 
 echo $PIN > /usr/local/.PIN.txt
 
-# Update inventory to populat ethe Extension Attribute
-
+# Update inventory to populate the Extension Attribute
 jamf recon
 
 # Delete PIN
-
 > /usr/local/.PIN.txt
 
 while  [[ $UserPin != $PIN ]] && [[ $Attempt -lt $MaxAttempt ]]
 
 do
-		
 # Request PIN from enduser
 Attempt=$(( Attempt +1 ))
-echo "$Attempt Attempt out of $MaxAttempt"
-		
-read -r -d '' applescriptCode <<'EOF'
-set UserPin to text returned of (display dialog "Please enter the PIN Provided by your Service Desk" default answer "" with title "Admin rights")
-return UserPin
-EOF
-		
-UserPin=$(osascript -e "$applescriptCode")
-		
-if [ "$?" != "0" ] ; then
-echo "User aborted. Exiting..."
-exit 0
-fi
-		
+logger "$Attempt Attempt out of $MaxAttempt"
+
+PINPrompt
+
 done
 
 if [[ "$UserPin" == "$PIN" ]]; then
 
-echo "Correct PIN Provided, granting Temporary Admin Rights"
+logger "Correct PIN Provided, granting Temporary Admin Rights"
 		
 # Get username of current logged in user
 USERNAME=$(/bin/echo 'show State:/Users/ConsoleUser' | /usr/sbin/scutil | /usr/bin/awk '/Name / { print $3 }')
@@ -162,30 +193,13 @@ fi
 done
 exit $?" | at -t "$(date -v+"$TEMPSECONDS"S "+%Y%m%d%H%M.%S")"
 			
-# Makes the user an admin
+# Make the user an admin
 /usr/sbin/dseditgroup -o edit -a "$USERNAME" -t user admin
 logger "Elevating $USERNAME."
 			
-# Path to Jamf Helper
-JAMFHELPERPATH="/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper"
-			
-# Displays a window showing how much time is left as an admin using Jamf Helper	
-			"$JAMFHELPERPATH" -windowType utility \
-			-windowPosition ur \
-			-title "Elevate User Account" \
-			-heading "Temporary Admin Rights Granted" \
-			-alignHeading middle \
-			-description "Please perform required administrative tasks" \
-			-alignDescription natural \
-			-icon "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/UnlockedIcon.icns" \
-			-iconSize 36 \
-			-button1 "Done" \
-			-defaultButton 1 \
-			-timeout "$TEMPSECONDS" \
-			-countdown \
-			-countdownPrompt "Admin Rights will be revoked in " \
-			-alignCountdown center
-			
+# Display a window showing how much time is left as an admin using Jamf Helper	
+ElevationCompletePrompt
+
 # Writes in logs when it's done
 logger "Elevation complete."
 exit 0
@@ -193,16 +207,14 @@ fi
 		
 # If user is already an admin, we write this in logs, tell the user and then quit
 logger "User already has elevated privileges."
-osascript -e "display dialog \"You already have elevated privileges \" buttons \"OK\" with icon caution"
+AlreadyAdminPrompt
 		
 fi
 
-
 if [[ "$UserPin" != "$PIN" ]]; then
-	
-echo "Wrong PIN provided $MaxAttempt times"
-	
-osascript -e "display dialog \"Incorrect PIN. Please contact your Servicedesk to obtain a PIN\""
+
+logger "Incorrect PIN Provided $MaxAttempt times"
+IncorrectPINPrompt
 	
 fi
 
